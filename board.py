1ed4c4982f95dac94c060ad36a3b5febd8ab391e
AdiJoost <adi.joost@gmail.com>
2022-04-07 14:33:35 +0200

    board now filled with fish

--- board.py
+++ board.py
@@ -5,6 +5,7 @@ class Board():
     all_fish = []
     start_position = 1
     save_position = 10
+    boat_position = 0
     game_dice = -1
     
     def __init__ (self, amount_of_fish, start_position, save_position,\
@@ -12,15 +13,21 @@ class Board():
         self.start_position = start_position
         self.save_position = save_position
         self.game_dice = dice.Dice(amount_of_fish, amount_of_fisher)
+        self.fill_fish(amount_of_fish)
         
     def roll_dice(self):
         print(self.game_dice.roll())
+        
+    def fill_fish(self, amount_of_fish):
+        for i in range (amount_of_fish):
+            self.all_fish.append(fish.Fish(f"Fish {i}", self.start_position))
     
     def to_string(self):
         return f"***BOARD***\n"\
                 f"Amount of fish: {len(self.all_fish)}\n"\
                 f"start_position: {self.start_position}\n"\
                 f"save_position: {self.save_position}"
+                
     
     def end_game(self):
         pass
@@ -29,7 +36,8 @@ class Board():
     
 board = Board(6,5,10,2)
 
-for i in range (5):
-    board.roll_dice()
     
 print(board.to_string())
+
+for i in range(len(board.all_fish)):
+    print(board.all_fish[i].to_string())
