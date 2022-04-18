dd73c2ea4c930a7cccb21947a7150e631e25b4aa
Adi Joost <adi.joost@gmail.com>
2022-04-18 12:03:46 +0200

    writer now displayed as a function

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 from board import Board
 import csv
+import os
+
+
+def write_to_csv ():
+    #path to csv-file
+    file_path = my_path + f"{amount_of_fish}{start_position}{save_position}"\
+           f"{amount_of_fisher}{int(hard_mode)}"
+           
+    #if file doesn't exist, create it and write headers
+    if not (os.path.isfile(file_path)):
+        with open (file_path, "a", encoding="UTF-8", newline='') as f:
+    
+            writer = csv.writer(f)
+            writer.writerow(header)
+            print("Row written")
+    
+    #opens csv and writes results on it
+    with open (file_path, "a", encoding="UTF-8", newline='') as f:
+    
+        writer = csv.writer(f)
+        writer.writerow(my_board.get_state())
+        print("Row written")
+
 
 amount_of_fish = 4
-start_position = 5
+start_position = 8
 save_position = 10
 amount_of_fisher = 2
 hard_mode = False
@@ -19,9 +42,5 @@ my_board = Board(amount_of_fish=amount_of_fish,
 my_board.start_game()
 print(my_board.get_state())
 
-with open (my_path + f"{amount_of_fish}{start_position}{save_position}"\
-           f"{amount_of_fisher}{int(hard_mode)}", "a", encoding="UTF-8", newline='') as f:
-    
-    writer = csv.writer(f)
-    writer.writerow(my_board.get_state())
-    print("Row written")
+write_to_csv()
+
