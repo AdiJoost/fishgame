44dc8a9cc834e23439fe4d07dc39336344a15bc6
Adi Joost <adi.joost@gmail.com>
2022-04-19 20:08:05 +0200

    now evaluates how many times a given play caught each fish

--- reader.py
+++ reader.py
@@ -10,11 +10,23 @@ def read_file (my_path):
             data.append(row)
     return headers, data
 
+def evaluate_catchrate(header, data):
+    games_played = len(data)
+    amount_of_fish = data[0][0]
+    catch_list = {}
+    for i in range(int(amount_of_fish) + 1):
+        catch_list[i] = 0
+    for row in data:
+        catch_list[int(row[2])] += 1
+    return games_played, amount_of_fish, catch_list
+
 
 my_path = "games_played/411020.csv"
 
 header, data = read_file(my_path)
 
-print(header)
-for row in data:
-    print(row)
+games_played, amount_of_fish, catch_list = evaluate_catchrate(header, data)
+
+print(games_played)
+print(amount_of_fish)
+print(catch_list)
